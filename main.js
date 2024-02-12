0042fb5584a3ef919fbc34de38df751d37231122
vvladl <vvlad@yandex.ru>
2024-02-12 20:10:04 +0300

    Changes to be committed: 	modified:   index.html 	modified:   main.js 	modified:   style.css me control added (go, left, right, stop)

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.getElementById('canvas')
 const c = canvas.getContext('2d')
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 400;
+canvas.height = 400;
+
+const b_go = document.getElementById('go')
+const b_left = document.getElementById('left')
+const b_right = document.getElementById('right')
+
 const dpi = Math.PI * 2
 const sw = 10 // scene width 
 const sh = 10 // scene height
 const bd = 40 // block dimension 
 const pi = Math.PI
-const anv =20 // angle of view 
+const anv = 20 // angle of view 
 let scene = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
@@ -24,9 +29,15 @@ let scene = [
 const me = {
   x: 5 * bd + 30,
   y: 5 * bd + 15,
-  d: 1,
-  draw: function drawMe() {
+  d: 1,            // direction
+  v: 0.2,          // speed
+  go: false,       // go or stop
+  draw: function () {
     c.fillStyle = 'blue'
+    if (this.go) {
+      this.x += this.v * Math.cos(this.d)
+      this.y += this.v * Math.sin(this.d)
+    }
     c.fillRect(this.x - 2, this.y - 2, 4, 4)
   }
 }
@@ -75,6 +86,7 @@ function entersaction(angle) {
     j = Math.floor(vx / bd)
     dd += dd_temp
     if (scene[i][j] == 1) {
+      if ((angle == me.d) && (dd_temp < 5)) me.go = false
       return { x: vx, y: vy }
     }
   }
@@ -95,7 +107,7 @@ function drawScene() {
   }
 }
 
-addEventListener('click', e => {
+canvas.addEventListener('click', e => {
   let dx = e.clientX - me.x
   let dy = e.clientY - me.y
   me.d = Math.atan(dy / dx)
@@ -105,6 +117,40 @@ addEventListener('click', e => {
   if (dx > 0 && dy < 0) me.d = dpi + me.d
 })
 
+b_left.addEventListener('click', () => {
+  if ((me.d -= 0.1) < 0) me.d = dpi - me.d
+})
+
+b_right.addEventListener('click', () => {
+  if ((me.d += 0.1) > dpi) me.d = me.d - dpi
+})
+
+b_go.addEventListener('click', e => {
+  me.go = !me.go
+  if (me.go) e.target.innerText = "Stop"
+  else e.target.innerText = "Go"
+})
+
+addEventListener('keydown', e => {
+  switch (e.key) {
+    case "ArrowUp":
+      me.go = true
+      b_go.innerText = "Stop"
+      break;
+    case "ArrowDown":
+      me.go = false
+      b_go.innerText = "Go"
+      break;
+    case "ArrowLeft":
+      if ((me.d -= 0.1) < 0) me.d = dpi - me.d
+      break;
+    case "ArrowRight":
+      if ((me.d += 0.1) > dpi) me.d = me.d - dpi
+      break;
+    default:
+      break;
+  }
+})
 
 function frames() {
   c.clearRect(0, 0, canvas.width, canvas.height)
